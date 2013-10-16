16c782c6baf305a3d064047b46ca9ab06ae63ad9
achmudas <achmudass@gmail.com>
2013-10-17 00:40:49 +0300

    Added native libs for windows

--- KillerPong/src/net/killerpong/main/KillerPong.java
+++ KillerPong/src/net/killerpong/main/KillerPong.java
@@ -1,16 +1,36 @@
 package net.killerpong.main;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
+/**
+ * 
+ * @author MkA
+ *
+ */
 public class KillerPong extends BasicGame {
 
-	public KillerPong(String title) {
-		super(title);
+	public KillerPong() {
+		super("Killer Pong");
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void main(String[] arguments)
+    {
+        try
+        {
+            AppGameContainer app = new AppGameContainer(new KillerPong());
+            app.setDisplayMode(800, 600, false);
+            app.start();
+        }
+        catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
